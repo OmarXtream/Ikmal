d548fd8bc7cdf0020e0caf9531ef899d39fbfb02
OmarXtream <48177756+OmarXtream@users.noreply.github.com>
2023-02-23 01:08:50 +0300

    Update navbar.blade.php

--- resources/views/frontend/partials/navbar.blade.php
+++ resources/views/frontend/partials/navbar.blade.php
@@ -66,7 +66,7 @@
                 <div class="col">
                     <div class="site-logo-wrap">
                         <div class="site-logo">
-                            <a href="index.html"><img src="{{asset('frontend/img/logo-2.png')}}" alt="Logo"></a>
+                            <a href="{{ route('home') }}"><img src="{{asset('frontend/img/logo-2.png')}}" alt="Logo"></a>
                         </div>
                         <div class="get-support clearfix d-none">
                             <div class="get-support-icon">
@@ -164,7 +164,7 @@
     <div class="ltn__utilize-menu-inner ltn__scrollbar">
         <div class="ltn__utilize-menu-head">
             <div class="site-logo">
-                <a href="index.html"><img src="{{asset('frontend/img/logo-2.png')}}" alt="Logo"></a>
+                <a href="{{ route('home') }}"><img src="{{asset('frontend/img/logo-2.png')}}" alt="Logo"></a>
             </div>
             <button class="ltn__utilize-close">×</button>
         </div>
