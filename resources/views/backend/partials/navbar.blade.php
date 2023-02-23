9620a2a6ff4f1f60947a7eb6be4f5ef4df1ca8cc
OmarXtream <48177756+OmarXtream@users.noreply.github.com>
2023-02-23 14:44:05 +0300

    title & favicon

--- resources/views/backend/partials/navbar.blade.php
+++ resources/views/backend/partials/navbar.blade.php
@@ -4,7 +4,7 @@
         <div class="navbar-header">
             <a href="javascript:void(0);" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse" aria-expanded="false"></a>
             <a href="javascript:void(0);" class="bars"></a>
-            <a class="navbar-brand" href="{{ route('admin.dashboard') }}">روشم</a>
+            <a class="navbar-brand" href="{{ route('admin.dashboard') }}">إكمال</a>
         </div>
 
         <div class="collapse navbar-collapse" id="navbar-collapse">
