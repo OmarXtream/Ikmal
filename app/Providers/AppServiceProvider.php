aed7d32600a523a4383024cf5963a8d9ae0b7a5f
OmarXtream <48177756+OmarXtream@users.noreply.github.com>
2023-02-26 18:15:48 +0300

    Force HTTPS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,9 @@ class AppServiceProvider extends ServiceProvider
     {
         Schema::defaultStringLength(191);
 
+        if(config('app.env') === 'production') {
+            \URL::forceScheme('https');
+        }
         if (! $this->app->runningInConsole()) {
 
             // SHARE TO ALL ROUTES
